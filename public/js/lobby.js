58d2998e3abea75e49d8b71b787498f79c7994b6
jesseharold <jesseharold@gmail.com>
2017-01-31 21:53:18 -0800

    building ready users queue in firebase - not finished

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,7 +1,25 @@
 // js for submitting a chat, getting screenname from hbrs
 // js for creating a pair of users, and sending them to createsession
    $(document).ready(function(){
-    firebase.initializeApp(config);
-	var database = firebase.database();
-    createChatRoom("lobby", 1000, user.displayName, database);
+        firebase.initializeApp(config);
+        var database = firebase.database();
+        createChatRoom("lobby", 1000, user.displayName, database);
+        $("#joinQueue").click(function(){
+            $(this).hide().next("p").text("Please wait...pairing you with someone...");
+
+            // add user.displayName to queue
+            var queueRef = database.ref("queue");
+            var meInQueueRef = queueRef.push(user.displayName);   
+            meInQueueRef.onDisconnect().remove();
+
+            //check to see if you can pair the user with someone else in the queue
+            queueRef.on("value", function(snapshot){
+                length = snapshot.numChildren();
+                console.log(length + "users in the queue");
+                if (length > 1){
+                    
+                }
+            });
+        });
+
    });
