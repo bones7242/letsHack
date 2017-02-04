b37c9d60beea2305506e420d9425c215a45ac8c3
jesseharold <jesseharold@gmail.com>
2017-02-03 20:40:04 -0800

    adding date to chats

--- public/js/chat.js
+++ public/js/chat.js
@@ -26,8 +26,8 @@ function createChatRoom(chatRoomName, maxUsers, myUserName, database){
             }
             var txt = '<span class="chatter">' + chat.screenname + ": </span>";
             txt += chat.message;
-            var chatTime = "<span class='chatTime'>" + chat.timestamp + "</span>";
-            div.html(txt);
+            var chatTime = "<span class='chatTime'>" + convertTime(chat.timestamp) + "</span>";
+            div.html(txt + chatTime);
             $("#chat-history").prepend(div);	
         }
     };
@@ -62,4 +62,20 @@ function createChatRoom(chatRoomName, maxUsers, myUserName, database){
     }).on("focusout", function(){
         $("body").off("keypress");
     });
+}
+
+function convertTime(unix_timestamp){
+    // Create a new JavaScript Date object based on the timestamp
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    var date = new Date(unix_timestamp*1000);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
 }
