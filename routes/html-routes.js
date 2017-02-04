b732642ae5abb9cfb58d82ccdfeba1dd95957511
jesseharold <jesseharold@gmail.com>
2017-02-03 23:11:09 -0800

    back to bare bones dummy route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
   app.get("/session/create", function(request, response){
-    var userId = request.query.userId;
-    var teammateId = request.query.teammateId;
-    var matchId = request.query.matchId;
-    console.log(teammateId);
+    //var userId = request.query.userId;
+    //var teammateId = request.query.teammateId;
+    //var matchId = request.query.matchId;
+   // console.log(teammateId);
    response.render('challenge');
   });
 };
