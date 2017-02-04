4df3dfe0ac2ed0ca456bab3cf53a340c79a946ca
bill bittner <bittner.w@gmail.com>
2017-02-04 07:10:35 -0800

    common

--- db/seeds.js
+++ db/seeds.js
@@ -2,84 +2,75 @@ var db = require("../models");
 
 module.exports = {
   createSeeds: function(){
-    // create users
-    // db.User.create({
-    //   email: "johndoe@gmail.com",
-    //   password: "password",
-    //   displayName: "john-doe",
-    //   firstName: "John",
-    //   lastName: "Doe"
-    // });
-    // db.User.create({
-    //   email: "janedoe@gmail.com",
-    //   password: "password",
-    //   displayName: "jane-doe",
-    //   firstName: "Jan",
-    //   lastName: "Doe"
-    // });
-    // db.User.create({
-    //   email: "jimidoe@gmail.com",
-    //   password: "password",
-    //   displayName: "jimi-doe",
-    //   firstName: "Jimi",
-    //   lastName: "Doe"
-    // });
     // create challenges
     db.Challenge.create({
         difficulty: 1,
             name: "Hello World",
-            instructionsAll: "Say 'Hello World!'",
+            instructionsAll: "Welcome to the wide world of programming.  First things first, say 'Hello World!'",
             instructionsA: "Create a function that returns the string 'Hello'",
             instructionsB: "Create a function that returns the string 'world!'",
-            startCodeA: "function sayHello(){ \m// A your code goes here \n};",
-            startCodeB: "function sayWorld(){ \m// B your code goes here \n};",
-            testA: "if (sayHello() === 'Hello') {return true} else {return false}",
-            testA: "if (sayWorld() === 'world!') {return true} else {return false}",
+            startCodeA: "function sayHello(){ // A your code goes here };",
+            startCodeB: "function sayWorld(){ // B your code goes here };",
+            testA: "Hello",
+            testA: "world!",
     });
     db.Challenge.create({
-        difficulty: 2,
-            name: "flip it and reverse it",
-            instructionsAll: "work together to solve this challenge! output should be bla",
-            instructionsA: "A your task is to spin the thing and return the other thing",
-            instructionsB: "B your task is to get all prime numbers betweeb bla and bla",
-            startCodeA: "// A your code goes here",
-            startCodeB: "// B code goes here",
-            test: "function(acode, bcode){return true;}"
+        difficulty: 1,
+            name: "Hello World",
+            instructionsAll: "Welcome to the wide world of programming.  First things first, say 'Hello World!'",
+            instructionsA: "Create a function that returns the string 'Hello'",
+            instructionsB: "Create a function that returns the string 'world!'",
+            startCodeA: "function sayHello(){ // A your code goes here };",
+            startCodeB: "function sayWorld(){ // B your code goes here };",
+            testA: "Hello",
+            testA: "world!",
     });
-    db.Challenge.create({
-        difficulty: 5,
-            name: "Just Another Challenge",
-            instructionsAll: "work together to solve this challenge! output should be bla",
-            instructionsA: "A your task is to spin the thing and return the other thing",
-            instructionsB: "B your task is to get all prime numbers betweeb bla and bla",
-            startCodeA: "// A your code goes here",
-            startCodeB: "// B code goes here",
-            test: "function(acode, bcode){return true;}"
-    });
+    // create users
+        // db.User.create({
+        //   email: "johndoe@gmail.com",
+        //   password: "password",
+        //   displayName: "john-doe",
+        //   firstName: "John",
+        //   lastName: "Doe"
+        // });
+        // db.User.create({
+        //   email: "janedoe@gmail.com",
+        //   password: "password",
+        //   displayName: "jane-doe",
+        //   firstName: "Jan",
+        //   lastName: "Doe"
+        // });
+        // db.User.create({
+        //   email: "jimidoe@gmail.com",
+        //   password: "password",
+        //   displayName: "jimi-doe",
+        //   firstName: "Jimi",
+        //   lastName: "Doe"
+        // });
     //create sessions
-    // db.Session.create({
-    //   success: true,
-    //   UserId: 1,
-    //   TeammateId: 2,
-    //   ChallengeId: 1
-    // });
-    // db.Session.create({
-    //   success: true,
-    //   UserId: 2,
-    //   TeammateId: 1,
-    //   ChallengeId: 1
-    // });
-    // db.Session.create({
-    //   success: false,
-    //   UserId: 1,
-    //   TeammateId: 2,
-    //   ChallengeId: 2
-    // });
-    // db.Session.create({
-    //   success: false,
-    //   UserId: 2,
-    //   TeammateId: 1,
-    //   ChallengeId: 2
-    // });
+        // db.Session.create({
+        //   success: true,
+        //   UserId: 1,
+        //   TeammateId: 2,
+        //   ChallengeId: 1
+        // });
+        // db.Session.create({
+        //   success: true,
+        //   UserId: 2,
+        //   TeammateId: 1,
+        //   ChallengeId: 1
+        // });
+        // db.Session.create({
+        //   success: false,
+        //   UserId: 1,
+        //   TeammateId: 2,
+        //   ChallengeId: 2
+        // });
+        // db.Session.create({
+        //   success: false,
+        //   UserId: 2,
+        //   TeammateId: 1,
+        //   ChallengeId: 2
+        // });
   }
 }
