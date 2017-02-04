b407a36483acd5b7caa1b26f99a745ae09db5924
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2017-02-04 11:29:57 -0800

    Hotfix for Update user on profile page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -60,16 +60,25 @@ module.exports = function(app) {
 
   // route for creating a session
   app.get("/session/create", function(req, res){
-    console.log("** post request received on /session/create.");
+    //console.log("** post request received on /session/create.");
+    //console.log("url", req.url);
     var userId = req.query.userId;
     var teammateId = req.query.teammateId;
     var matchId = req.query.matchId;
 
-    //var isPlayerA = req.query.isPlayerA;
+    if (req.query.isPlayerA == "true") {
+      var isPlayerA = true;
+      var isPlayerB = false;
+    } else {
+      var isPlayerA = false;
+      var isPlayerB = true;
+    };  // i need this from Harold
+    //console.log("userId:", userId);
+    //console.log("teammateId:", teammateId);
+    //console.log("matchId:", matchId);
+    console.log("isPlayerA:", isPlayerA);
+    //console.log(typeof(isPlayerA));
 
-    console.log("userId:", userId);
-    console.log("teammateId:", teammateId);
-    console.log("matchId:", matchId);
     // 1. select a challenge id that isn't in either user's challenge history.
     db.sequelize.Promise.all([
       db.Session.findAll({
@@ -108,8 +117,8 @@ module.exports = function(app) {
       // 2. create the session and get the information
       db.Session.create({
         success: "false",  // will always be false when created
-        playerA: false,
-        playerB: false,
+        playerA: isPlayerA,
+        playerB: isPlayerB,
         matchId: matchId,
         ChallengeId: challengeToUse,  // note: must be an valid(existing) ChallengeId
         UserId: userId,  // note: must be an valid(existing) UserId
@@ -132,10 +141,6 @@ module.exports = function(app) {
   // route for updating a user
   app.put("/user/update", function(req, res){
 
-    if (req.body.email === ''){
-
-    }
-
     //route to update a user
     db.User.update({
       email: req.body.email || db.User.email,
@@ -143,7 +148,9 @@ module.exports = function(app) {
       lastName: req.body.lastName || db.User.lastName
     }, {
       where: {
+
         displayName: req.body.displayName  // can change this to displayName or email if that is better
+
       }
     }).then(function(result){
         // console.log("this is user: " + user);
@@ -201,7 +208,8 @@ module.exports = function(app) {
       instructionsB: req.body.instructionsB,
       startCodeA: req.body.startCodeA,
       startCodeB: req.body.startCodeB,
-      test: req.body.test
+      testA: req.body.testA,
+      testB: req.body.testB
     }).then(function(newChallenge){
       res.json(newChallenge);
     }).catch(function (err) {
@@ -220,7 +228,8 @@ module.exports = function(app) {
       instructionsB: req.body.instructionsB,
       startCodeA: req.body.startCodeA,
       startCodeB: req.body.startCodeB,
-      test: req.body.test
+      testA: req.body.testA,
+      testB: req.body.testB
     }, {
       where: {
         id: req.body.id
