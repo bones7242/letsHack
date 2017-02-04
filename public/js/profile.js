e70ad1cd45be8af0df1d994079729ff36f24c0a6
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2017-02-04 03:53:16 -0800

    got profile edit buttons to stop refreshing page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     showChallengeHistory();
-    
+
     $(".profileSection button").click(function(){
         $(this).parent(".profileSection").find("input.profileEdit").show();
+        return false;
     });
 });
