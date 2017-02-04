22bf46c98d951f0b0d84e27f2f65dbb209842a1e
jesseharold <jesseharold@gmail.com>
2017-02-03 23:14:02 -0800

    dinosaur create

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  app.get("/session/create", function(request, response){
+  app.get("/dinosaurs/create", function(request, response){
     //var userId = request.query.userId;
     //var teammateId = request.query.teammateId;
     //var matchId = request.query.matchId;
