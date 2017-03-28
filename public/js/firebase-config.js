3c80c32a51741a66bfc722788894f8105c377780
jesseharold <jesseharold@gmail.com>
2017-03-28 16:34:15 -0700

    removing private for github deploy

--- public/js/firebase-config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    apiKey: "AIzaSyDIqjIxm4rHbVilBE6rxLXEwAhaBGae1MA",
-    authDomain: "pair-coding-challenge.firebaseapp.com",
-    databaseURL: "https://pair-coding-challenge.firebaseio.com",
-    storageBucket: "pair-coding-challenge.appspot.com",
-    messagingSenderId: "679472985478"
-};
