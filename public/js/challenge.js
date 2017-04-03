be7d3045ee428e39d21568de1f9c5682c21a762d
jesseharold <jesseharold@gmail.com>
2017-04-02 22:54:28 -0700

    added socket to manage session success, added options to codemirror

--- public/js/challenge.js
+++ public/js/challenge.js
@@ -8,14 +8,16 @@ $(document).ready(function() {
     // create a CodeMirror editor window:
     var cmcontainer = document.getElementById("code-mirror-container");
     var myCodeMirror = CodeMirror(cmcontainer, {
-        value: "function myScript(){\n\treturn 100;\n}",
-        mode:  "javascript"
+        value: $(".dataHolder").data().startcode,
+        mode:  "javascript",
+        lineSeparator: "\n",
+        tabSize: 2,
+        lineNumbers: true,
+        autofocus: true
     });
 
     // start up chat
     createChatRoom(matchId, 2, user.displayName);
-    //put my starter code into the textarea
-    $("#userCode").val(addBRTags($(".dataHolder").data().startcode));
 
     var socket = io();
 
@@ -43,17 +45,60 @@ $(document).ready(function() {
         }
     });
 
-    // perform code test
-    $("button.testMyCode").on("click", testMyCode);
+    socket.on("challengeHalfDone", function(halfDone){
+        if (halfDone.session == matchId){
+            // this event is for our current session
+            if (halfDone.userName == partnerName){
+                // your partner is done!
+                //console.log("your partner passed their test");
+                partnerPassedTest = true;
+                if (iPassedTest){
+                    challengeSuccess();
+                }
+            }
+        }
+    });
 
-    function addBRTags(input){
-        if (input && typeof input === "string" && input.length > 1){
-            return input.split("&#10;").join("\n");
+    // perform code test
+    $("button.testMyCode").on("click", function(){
+        testMyCode(myCodeMirror.getValue());
+    });
+
+    function testMyCode(userCode){
+        // Get this user's test, as passed down from the db
+        var challengeTest = $("input#myTest").val();
+        var passedTest = false;
+
+        try { 
+            var returnValue = eval("(" + userCode + ")()");
+            if (returnValue == challengeTest){
+                passedTest = true;
+            }
+        }
+        catch (err) {
+           openModal("Your code threw an error", err, "OK", closeModal);
+        }
+        finally {
+            if (passedTest) {
+                // update session
+                socket.emit("oneChallengePassed", {session: matchId, userName: user.displayName});
+                iPassedTest = true;
+                if (partnerPassedTest){
+                    //we both passed yay!
+                    challengeSuccess();
+                } else {
+                    // I passed, my parnter hasn't yet
+                    openModal("Nice work!", "Your partner is still working, see if you can help them out using the chat.", "OK", closeModal);
+                }
+            } else {
+                //i didn't pass
+                openModal("Your code didn't return the expected result.", "Keep trying!", "OK", closeModal);
+            }
         }
     }
 
     function challengeSuccess(){
-        console.log("calling challenge success");
+        //console.log("calling challenge success");
         // update the session record to show success
         $.ajax({
             type: "PUT",
@@ -64,49 +109,13 @@ $(document).ready(function() {
             },
             success: function(response){
                 if (response){
-                    console.log("session updated! ", response);
+                    //console.log("session updated! ", response);
                     openModal("Success!", "You both passed your challenge, nice team work, you guys! Head back to the lobby for more challenge fun!", "Lobby", function(){window.location = "/lobby"});
                 } else {
                     console.error("not able to update session", sessionData.sessionId);
                 }
             }
         });
-    }
-
-    function testMyCode(){
-        // Take the player's code
-        var userCode = $("#userCode").val();
-        // Get this user's test, as passed down from the db
-        var challengeTest = $("input#myTest").val();
-        var passedTest = false;
-
-    //    try { 
-        var returnValue = eval("(" + userCode + ")()");
-        if (returnValue == challengeTest){
-            passedTest = true;
-        }
-  //      }
-  //      catch (err) {
-  //          openModal("Your code threw an error", err, "OK", closeModal);
-  //      }
-  //      finally {
-        if (passedTest) {
-            myRef.update({
-                finished: 1
-            });
-            iPassedTest = true;
-            if (partnerPassedTest){
-                //we both passed yay!
-                challengeSuccess();
-            } else {
-                // I passed, my parnter hasn't yet
-                openModal("Nice work!", "Your partner is still working, see if you can help them out using the chat.", "OK", closeModal);
-            }
-        } else {
-            //i didn't pass
-            openModal("Your code didn't return the expected result.", "Keep trying!", "OK", closeModal);
-        }
-        //}
     }
 
 });
